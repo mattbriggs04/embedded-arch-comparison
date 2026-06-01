38cd97d4c25a1ab01c91236852d73ab2ea60e5ae
Matthew Briggs <matthew.briggs04@gmail.com>
2026-05-31 22:54:03 -0700

    button.c added

---
+++ baremetal-cmsis/src/button.c
@@ -0,0 +1,7 @@
+
+// initialize the button as an external interrupt source
+int init_button() {
+
+}
+
+// void button_irq <replace with name>
--- baremetal-cmsis/src/main.c
+++ baremetal-cmsis/src/main.c
@@ -1,4 +1,12 @@
+#include "stm32f303x8.h"
+
+#define BTN_PIN
 
 int main(void) {
 
+    // Baremetal: superloop method
+    // since no RTOS or Async framework
+    while (1) {
+
+    }
 }
